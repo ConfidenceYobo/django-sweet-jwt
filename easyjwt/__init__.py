d59140b5333456dc4b5487bbdfa2e999499fea8a
Confi Yobo <confiyobo@gmail.com>
2019-12-14 09:45:12 +0100

    first commit

---
+++ easyjwt/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'djangoeasyjwt'
+__version__ = '0.0.1'
+__author__ = 'Confi Yobo'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014-2017 Blimp LLC'
+
+# Version synonym
+VERSION = __version__
